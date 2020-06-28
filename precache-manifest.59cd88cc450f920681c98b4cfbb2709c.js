75077a6486caf2fc32468c2e6cb32e2cd452a07a
emilygarvey <emilyg@scripted.org>
2020-06-28 12:23:04 -0400

    Updates

---
+++ precache-manifest.59cd88cc450f920681c98b4cfbb2709c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47476695ef5dd5692251b46f2a2e5b0",
+    "url": "/react-three-fiber-starter/index.html"
+  },
+  {
+    "revision": "b02107a64b473d68095d",
+    "url": "/react-three-fiber-starter/static/js/2.2f0fcd70.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/react-three-fiber-starter/static/js/2.2f0fcd70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315f70fac790e77b0408",
+    "url": "/react-three-fiber-starter/static/js/main.a6b3d7a7.chunk.js"
+  },
+  {
+    "revision": "69843cc7bef9bd272b04",
+    "url": "/react-three-fiber-starter/static/js/runtime-main.d4463acb.js"
+  }
+]);
